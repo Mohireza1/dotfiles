8e19b09a16311f8a92d5c7d87cce73699047a8b7
mohireza <mohireza50@gmail.com>
2026-07-31 08:31:52 +0330

    adjust niri keybinds

--- hypr/refresh_rate.sh
+++ hypr/refresh_rate.sh
@@ -4,15 +4,17 @@ CONFIG_FILE="$HOME/.config/niri/config.kdl"
 
 # Check if current config has 165Hz
 if grep -q "165.008" "$CONFIG_FILE"; then
-    # Switch to 60Hz
+    # Switch to 60Hz and disable VRR
     sed -i 's/165\.008/60.000/g' "$CONFIG_FILE"
+    sed -i 's/.*variable-refresh-rate.*/    \/\/ variable-refresh-rate/g' "$CONFIG_FILE"
     niri msg action load-config-file
-    notify-send -u normal "Display" "Switched to 60 Hz mode"
+    notify-send -u normal "Display" "Switched to 60 Hz mode (VRR Off)"
 elif grep -q "60.000" "$CONFIG_FILE"; then
-    # Switch to 165Hz
+    # Switch to 165Hz and enable VRR
     sed -i 's/60\.000/165.008/g' "$CONFIG_FILE"
+    sed -i 's/.*variable-refresh-rate.*/    variable-refresh-rate/g' "$CONFIG_FILE"
     niri msg action load-config-file
-    notify-send -u normal "Display" "Switched to 165 Hz mode"
+    notify-send -u normal "Display" "Switched to 165 Hz mode (VRR On)"
 else
     notify-send -u critical "Display Error" "Could not detect current refresh rate in Niri config"
     exit 1
